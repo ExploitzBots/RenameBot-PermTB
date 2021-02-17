1038be1abfd6bbb3a4207ed68e867fd009bba9e7
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-02-17 21:27:30 +0530

    script.py

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 class script(object):
     START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
 
-<b>Send me any Telegram file and choose appropriate option! </b>"""
+<b>Send me any Telegram file and choose appropriate option!. This Bot Is Made With ❤️ By @ExploitzBots </b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
+    UPGRADE_TEXT = "CONTACT @Madboi_xD"
     DOWNLOAD_START = "Give Me Some Time..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @ExploitzBots **"
     SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
